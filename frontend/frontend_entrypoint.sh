1b3533c551f45b96e2934d24be802f260af9a68f
Naoki Akazawa <naoki9akazawa@gmail.com>
2020-02-01 13:00:53 +0900

    modify frontend environment

--- frontend/frontend_entrypoint.sh
+++ frontend/frontend_entrypoint.sh
@@ -6,8 +6,19 @@ if [ ! -d $DIR ];then
     echo -e "\n===> creating a react.js project\n"
     npx create-react-app dapp
     cd /usr/app/src/dapp
-    echo -e "\n===> installing material-ui & web3.js\n"
-    npm install web3@1.2.4 websocket@1.0.30 @material-ui/core @material-ui/icons
+    echo -e "\n===> installing UI tools & web3.js\n"
+    npm install web3@1.2.4 websocket@1.0.30 @material-ui/core @material-ui/icons axios@0.19.1 react-router@5.1.2 react-router-dom@5.1.2 onsenui@2.10.10 react-onsenui auth0-js@9.12.2 http-proxy-middleware@0.20.0
+    echo -e "\n===> Stand by OK!!\n"
 fi
-cd /usr/app/src/dapp
-npm start
+
+DIR_Modules="/usr/app/src/dapp/node_modules"
+if [ ! -d $DIR_Modules ];then
+    cd /usr/app/src/dapp
+    echo -e "\n===> installing UI tools & web3.js\n"
+    npm install
+    echo -e "\n===> Stand by OK!!\n"
+fi
+/bin/bash
+
+#cd $DIR
+#npm start
